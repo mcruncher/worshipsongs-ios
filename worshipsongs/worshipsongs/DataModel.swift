5e12f4edea34308f54bb6d518e6ccc6927cb3748
Seenivasan <mailtonivass@gmail.com>
2014-12-18 11:17:53 +0800

    #2 Support to created to simple table view with search options

---
+++ worshipsongs/worshipsongs/DataModel.swift
@@ -0,0 +1,15 @@
+//
+//  DataModel.swift
+//  worshipsongs
+//
+//  Created by Seenivasan Sankaran on 12/17/14.
+//  Copyright (c) 2014 Seenivasan Sankaran. All rights reserved.
+//
+
+import Foundation
+
+
+struct DataModel {
+    let category : String
+    let name : String
+}
---
+++ worshipsongs/worshipsongs/MyPlayground.playground/section-1.swift
@@ -0,0 +1,34 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var fifaTeams = ["Brazil", "Croatia", "Mexico", "Cameroon", "Spain", "Netherlands", "Chile", "Australia","Columbia", "Greece", "Ivory Coast", "Japan", "Uruguay", "Costa Rica", "England", "Italy","Switzerland", "Ecuador", "France", "Honduras", "Argentina", "Bosnia-Herzegovia", "Iran","Nigeria","Germany", "Portugal", "Ghana", "USA", "Belgium", "Algeria", "Russia", "Korea"]
+
+var searchResultsOfFifaTeams = fifaTeams
+
+func searchTeamName(searchText: String){
+    
+    for teamName in fifaTeams
+        
+    {
+        
+        if (teamName as NSString).localizedCaseInsensitiveContainsString(searchText)
+            
+        {
+            
+            //searchResultsOfFifaTeams.append(teamName)
+            let selectedTeam = teamName
+            
+        }
+        
+    }
+    
+}
+
+
+searchTeamName("Ir")
+
+
+
