8ccba7544d07990fd8f55fde45b53c881fd29579
Seenivasan <mailtonivass@gmail.com>
2014-12-26 10:35:32 +0800

    #11 Created Font Setting view controller and Color Setting View controller

---
+++ worshipsongs/worshipsongs/Classes/ColorSettingsViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ColorSettingsViewController.swift
+//  worshipsongs
+//
+//  Created by Seenivasan Sankaran on 12/24/14.
+//  Copyright (c) 2014 Seenivasan Sankaran. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+
+class ColorSettingsViewController: UITableViewController {
+    
+    var primaryLanguageColorCell: UITableViewCell = UITableViewCell()
+    var secondaryLanguageColorCell: UITableViewCell = UITableViewCell()
+    
+    
+    var primaryLanguageLabel: UILabel = UILabel()
+    var primaryLanguageColorLabel: UILabel = UILabel()
+    var secondaryLanguageLabel: UILabel = UILabel()
+    var secondaryLanguageColorLabel: UILabel = UILabel()
+    
+    var primaryLanguageColor: UIColor = UIColor()
+    
+    var colorView: UIViewController = UIViewController()
+   
+    
+    override func loadView() {
+        super.loadView()
+        
+        // set the title
+        self.title = "Color Settings"
+        
+        //CGRect Label1Frame = CGRectMake(10, 10, 290, 25);
+        //CGRect Label2Frame = CGRectMake(10, 33, 290, 25);
+        
+        // construct font setting cell, section 0, row 0
+        self.primaryLanguageColorCell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+        self.primaryLanguageLabel = UILabel(frame: CGRectMake(10, 10, 250, 25))
+        self.primaryLanguageLabel.text = "Choose primary font color"
+        self.primaryLanguageLabel.font = getFont()
+        self.primaryLanguageColorCell.addSubview(self.primaryLanguageLabel)
+        
+        primaryLanguageColorLabel = UILabel(frame: CGRectMake(260, 15, 10, 10))
+        primaryLanguageColorLabel.backgroundColor = UIColor.redColor()
+        self.primaryLanguageColorCell.addSubview(primaryLanguageColorLabel)
+        
+        // construct color setting cell, section 0, row 1
+        self.secondaryLanguageColorCell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+        self.secondaryLanguageLabel = UILabel(frame: CGRectMake(10, 10, 250, 25))
+        self.secondaryLanguageLabel.text = "Choose secondary font color"
+        self.secondaryLanguageLabel.font = getFont()
+        self.secondaryLanguageColorCell.addSubview(self.secondaryLanguageLabel)
+        
+
+        secondaryLanguageColorLabel = UILabel(frame: CGRectMake(260, 15, 10, 10))
+        secondaryLanguageColorLabel.backgroundColor = UIColor.redColor()
+        self.secondaryLanguageColorCell.addSubview(secondaryLanguageColorLabel)
+        
+    }
+    
+    // Return the number of sections
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    // Return the number of rows for each section in your static table
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch(section) {
+        case 0: return 1    // section 0 has 2 rows
+        case 1: return 1    // section 1 has 1 row
+        default: fatalError("Unknown number of sections")
+        }
+    }
+    
+    // Return the row for the corresponding section and row
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        switch(indexPath.section) {
+        case 0:
+            switch(indexPath.row) {
+            case 0: return self.primaryLanguageColorCell    // section 0, row 0 is the fontSettingsCell
+            default: fatalError("Unknown row in section 0")
+            }
+        case 1:
+            switch(indexPath.row) {
+            case 0: return self.secondaryLanguageColorCell // section 1, row 0 is the keepAwakeCell option
+            default: fatalError("Unknown row in section 1")
+            }
+            
+        default: fatalError("Unknown section")
+        }
+    }
+    
+    // Customize the section headings for each section
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch(section) {
+        case 0: return "Primary Language Color"
+        case 1: return "Secondary Language Color"
+        default: fatalError("Unknown section")
+        }
+    }
+    
+    // Configure the row selection code for any cells that you want to customize the row selection
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if(indexPath.section == 0 && indexPath.row == 0) {
+            self.colorView.view.hidden = false
+            makeColorView(1)
+        }
+        if(indexPath.section == 1 && indexPath.row == 0) {
+            self.colorView.view.hidden = false
+            makeColorView(2)
+        }
+    }
+    
+    func makeColorView(var colorTag:Int){
+        var buttonFrame = CGRect(x: 12, y: 100, width: 25, height: 25)
+        var i:CGFloat = 1.0
+        while i > 0{
+            makeRainbowButtons(buttonFrame, sat: i ,bright: 1.0, colorTag: colorTag)
+            i = i - 0.1
+            buttonFrame.origin.y = buttonFrame.origin.y + buttonFrame.size.height
+        }
+        self.addChildViewController(colorView)
+        self.colorView.view.alpha = 0;
+        self.colorView.view.opaque = true;
+        self.colorView.didMoveToParentViewController(self)
+        self.colorView.view.frame = self.view.frame
+        self.view.addSubview(colorView.view)
+        UIView.animateWithDuration(0.25, delay: 0.0, options: .CurveLinear, animations: {
+            self.colorView.view.alpha = 1;
+            }, completion: nil)
+    }
+    
+    func makeRainbowButtons(buttonFrame:CGRect, sat:CGFloat, bright:CGFloat, colorTag:Int){
+        var myButtonFrame = buttonFrame
+        //populate an array of buttons
+        for i in 0..<12{
+            let hue:CGFloat = CGFloat(i) / 12.0
+            let color = UIColor(hue: hue, saturation: sat, brightness: bright, alpha: 1.0)
+            let aButton = UIButton(frame: myButtonFrame)
+            myButtonFrame.origin.x = myButtonFrame.size.width + myButtonFrame.origin.x
+            aButton.backgroundColor = color
+            aButton.tag = colorTag
+            colorView.view.addSubview(aButton)
+            aButton.addTarget(self, action: "displayColor:", forControlEvents: UIControlEvents.TouchUpInside)
+        }
+    }
+    
+    func displayColor(sender:UIButton){
+        var r:CGFloat = 0,g:CGFloat = 0,b:CGFloat = 0
+        var a:CGFloat = 0
+        var h:CGFloat = 0,s:CGFloat = 0,l:CGFloat = 0
+        let color = sender.backgroundColor!
+        if color.getHue(&h, saturation: &s, brightness: &l, alpha: &a){
+            if color.getRed(&r, green: &g, blue: &b, alpha: &a){
+                var colorText = NSString(format: "HSB: %4.2f,%4.2f,%4.2f RGB: %4.2f,%4.2f,%4.2f",
+                    Float(h),Float(s),Float(b),Float(r),Float(g),Float(b))
+                primaryLanguageColor = UIColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: CGFloat(a))
+                let tagValue = sender.tag
+                if(tagValue == 1)
+                {
+                    primaryLanguageColorLabel.backgroundColor = primaryLanguageColor
+                }
+                else
+                {
+                    secondaryLanguageColorLabel.backgroundColor = primaryLanguageColor
+                }
+                self.colorView.view.hidden = true
+                
+            }
+        }
+        
+    }
+    
+    func getFont() -> UIFont{
+        return UIFont(name: "HelveticaNeue", size: CGFloat(12))!
+    }
+}
---
+++ worshipsongs/worshipsongs/Classes/FontSettingsViewController.swift
@@ -0,0 +1,147 @@
+//
+//  FontSettingsViewController.swift
+//  worshipsongs
+//
+//  Created by Seenivasan Sankaran on 12/23/14.
+//  Copyright (c) 2014 Seenivasan Sankaran. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FontSettingsViewController: UITableViewController, UIPickerViewDelegate,UIPickerViewDataSource, UITextFieldDelegate, UIActionSheetDelegate {
+    
+    var pickerView:UIPickerView = UIPickerView()
+    
+    var fontNameCell: UITableViewCell = UITableViewCell()
+    
+    var fontSizeSliderCell: UITableViewCell = UITableViewCell()
+    
+    var fontNameTextfield: UITextField = UITextField()
+    var fontSizeSlider: UISlider = UISlider()
+    var fontSizeLabel: UILabel = UILabel()
+     var elements:Array<String>?
+    
+    override func loadView() {
+        super.loadView()
+        
+        // set the title
+        self.title = "Font Settings"
+        
+        elements = ["One","Two","Three","Five","Six","Seven","Eight","Nine","Ten"]
+       
+        
+        // construct font setting cell, section 0, row 0
+        self.fontNameCell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+        self.fontNameTextfield = UITextField(frame: CGRectInset(self.fontNameCell.contentView.bounds, 15, 0))
+        self.fontNameTextfield.placeholder = "Select font"
+        self.fontNameTextfield.font = getFont()
+        self.fontNameTextfield.inputView = pickerView
+        self.fontNameTextfield.delegate = self
+        self.fontNameTextfield.tintColor=UIColor.clearColor()
+      //  self.fontNameTextfield. = false
+        self.fontNameCell.addSubview(self.fontNameTextfield)
+        
+        self.fontSizeSliderCell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+        self.fontSizeSlider = UISlider(frame: CGRectInset(self.fontSizeSliderCell.contentView.bounds, 50, 0))
+        self.fontSizeSlider.maximumValue = 50;
+        self.fontSizeSlider.minimumValue = 10;
+        self.fontSizeSlider.continuous = true;
+        self.fontSizeSlider.addTarget(self, action: "sliderChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        self.fontSizeLabel = UILabel(frame: CGRectInset(self.fontSizeSliderCell.contentView.bounds, 20, 0))
+        self.fontSizeLabel.text="15"
+        self.fontSizeSliderCell.addSubview(fontSizeLabel)
+        self.fontSizeSliderCell.addSubview(self.fontSizeSlider)
+        
+        self.pickerView.dataSource = self
+        self.pickerView.delegate = self
+        self.pickerView.showsSelectionIndicator = true
+        self.pickerView.hidden = false
+    }
+    
+    // Return the number of sections
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    // Return the number of rows for each section in your static table
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch(section) {
+        case 0: return 1    // section 0 has 2 rows
+        case 1: return 1    // section 1 has 1 row
+        default: fatalError("Unknown number of sections")
+        }
+    }
+    
+    // Return the row for the corresponding section and row
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        switch(indexPath.section) {
+        case 0:
+            switch(indexPath.row) {
+            case 0: return self.fontNameCell    // section 0, row 0 is the fontSettingsCell
+            default: fatalError("Unknown row in section 0")
+            }
+        case 1:
+            switch(indexPath.row) {
+            case 0: return self.fontSizeSliderCell // section 1, row 0 is the keepAwakeCell option
+            default: fatalError("Unknown row in section 1")
+            }
+            
+        default: fatalError("Unknown section")
+        }
+    }
+    
+    // Customize the section headings for each section
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch(section) {
+        case 0: return "Font name"
+        case 1: return "Font size"
+        default: fatalError("Unknown section")
+        }
+    }
+    
+    // Configure the row selection code for any cells that you want to customize the row selection
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func sliderChanged(sender:UISlider){
+        fontSizeLabel.text = String(format: "%.0f", fontSizeSlider.value)
+    }
+    
+    func getFont() -> UIFont{
+        return UIFont(name: "HelveticaNeue", size: CGFloat(12))!
+    }
+    
+    
+    // returns the number of 'columns' to display.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
+        return 1
+        
+    }
+    
+    // returns the # of rows in each component..
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return elements!.count
+    }
+    
+    func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String
+    {
+        return elements![row]
+    }
+    
+    func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int)
+    {
+        fontNameTextfield.text = elements![row]
+        pickerView.hidden = true
+    }
+    
+    
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        pickerView.hidden = false
+        return true
+    }
+    
+    
+}
