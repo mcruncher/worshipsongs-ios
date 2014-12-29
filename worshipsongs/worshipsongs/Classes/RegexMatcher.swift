5923a55778d4c936b8a466e8ae22dd68fd9f3d65
Seenivasan <mailtonivass@gmail.com>
2014-12-29 08:50:05 +0800

    #10 Created Regex matcher to find the custom tags

--- worshipsongs/worshipsongs/Classes/RegexMatcher.swift
+++ worshipsongs/worshipsongs/Classes/RegexMatcher.swift
@@ -8,48 +8,22 @@
 
 import Foundation
 
-infix operator =~ {}
-
-func =~ (value : String, pattern : String) -> RegexMatchResult {
+func getRange (value : String, pattern : String) -> NSMutableArray {
     var err : NSError?
     let nsstr = value as NSString // we use this to access the NSString methods like .length and .substringWithRange(NSRange)
     let options = NSRegularExpressionOptions(0)
     let re = NSRegularExpression(pattern: pattern, options: options, error: &err)
-    if let e = err {
-        return RegexMatchResult(items: [])
-    }
     let all = NSRange(location: 0, length: nsstr.length)
     let moptions = NSMatchingOptions(0)
     var matches : Array<String> = []
+    var array: NSMutableArray = NSMutableArray()
     re!.enumerateMatchesInString(value, options: moptions, range: all) {
         (result : NSTextCheckingResult!, flags : NSMatchingFlags, ptr : UnsafeMutablePointer<ObjCBool>) in
         let string = nsstr.substringWithRange(result.range)
         println("Range: \(result.range)")
-
-        matches.append(string)
+        array.addObject(result.range)
     }
-    return RegexMatchResult(items: matches)
+    return array
 }
 
-struct RegexMatchCaptureGenerator : GeneratorType {
-    mutating func next() -> String? {
-        if items.isEmpty { return nil }
-        let ret = items[0]
-        items = items[1..<items.count]
-        return ret
-    }
-    var items: Slice<String>
-}
 
-struct RegexMatchResult : SequenceType, BooleanType {
-    var items: Array<String>
-    func generate() -> RegexMatchCaptureGenerator {
-        return RegexMatchCaptureGenerator(items: items[0..<items.count])
-    }
-    var boolValue: Bool {
-        return items.count > 0
-    }
-    subscript (i: Int) -> String {
-        return items[i]
-    }
-}
