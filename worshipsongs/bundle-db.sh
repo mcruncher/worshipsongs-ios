a6479a237e1f88e8aa8d264b67c4d9f560cfb164
Vignesh Palanisamy <vignesh@mcruncher.com>
2016-08-24 16:47:25 +0800

    #56 Updated the script

--- worshipsongs/bundle-db.sh
+++ worshipsongs/bundle-db.sh
@@ -1,6 +1,7 @@
 
 git clone https://github.com/crunchersaspire/worshipsongs-db-dev.git bundle-db
 echo "Coping latest database ..."
+rm -rf worshipsongs/songs.sqlite
 cp -rf bundle-db/songs.sqlite worshipsongs
 rm -rf bundle-db
 echo "Removed bundle-db directory"
